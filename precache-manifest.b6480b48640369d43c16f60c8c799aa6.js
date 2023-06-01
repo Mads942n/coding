0642e1e038adb18087d3417c04203b1401d0b9cf
Mads942n <117907000+madsbruhn00@users.noreply.github.com>
2023-06-02 01:15:19 +0200

    Updates

---
+++ precache-manifest.b6480b48640369d43c16f60c8c799aa6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "184369572f67ab71a4f3",
+    "url": "/coding/finalexam/static/css/main.37416105.chunk.css"
+  },
+  {
+    "revision": "184369572f67ab71a4f3",
+    "url": "/coding/finalexam/static/js/main.18436957.chunk.js"
+  },
+  {
+    "revision": "6ab0c6d748d7531c8884",
+    "url": "/coding/finalexam/static/js/1.6ab0c6d7.chunk.js"
+  },
+  {
+    "revision": "6a60e3bde0ba2208a72e",
+    "url": "/coding/finalexam/static/js/runtime~main.6a60e3bd.js"
+  },
+  {
+    "revision": "c9094fd63ad77eda56a82266cbfd33b9",
+    "url": "/coding/finalexam/static/media/plismaskine.c9094fd6.svg"
+  },
+  {
+    "revision": "63d387bf537be97d47e502af795556d3",
+    "url": "/coding/finalexam/static/media/dynamicsettings.63d387bf.svg"
+  },
+  {
+    "revision": "2036f90d0da071feb2c0228a2a32442d",
+    "url": "/coding/finalexam/static/media/usbsettings.2036f90d.svg"
+  },
+  {
+    "revision": "9a29f5d2e07dc13dad50e3e7fb8cdb39",
+    "url": "/coding/finalexam/static/media/useradmin.9a29f5d2.svg"
+  },
+  {
+    "revision": "a956769baada1b812f663146153063e5",
+    "url": "/coding/finalexam/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/finalexam/precache-manifest.d0119d672ac2f98d2ee17eb0f0b2370c.js"
+  "/coding/finalexam/precache-manifest.b6480b48640369d43c16f60c8c799aa6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/finalexam/index.html", {
+workbox.routing.registerNavigationRoute("/coding/finalexam/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
